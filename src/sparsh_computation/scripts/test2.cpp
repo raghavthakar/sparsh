cbaa1ddce080edfa2f95a0b6aee72ef6df64f6c8
Blebot0 <keshav29kapur@gmail.com>
2020-12-16 23:14:08 +0530

    Added opencv

---
+++ src/sparsh_computation/scripts/test2.cpp
@@ -0,0 +1,30 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+
+using namespace std;
+using namespace cv;
+
+cv::Mat img, erosion_dst, dilation_dst;
+
+int main(int argc, char const *argv[])
+{
+
+
+    img = cv::imread("picture.png",cv::IMREAD_GRAYSCALE );
+    if( img.empty() )
+    {
+        cout << "Could not open or find the image!\n" << endl;
+        cout << "Usage: " << argv[0] << " <Input image>" << endl;
+        return -1;
+    }
+
+    cv::erode(img, erosion_dst, cv::Mat(), cv::Point(-1, -1),2, 1, 1);
+    cv::dilate(img, dilation_dst, cv::Mat(), cv::Point(-1, -1),2, 1, 1);
+
+
+    cv::imwrite("img_eroded.png", erosion_dst);
+    cv::imwrite("img_dilated.png", dilation_dst);
+    return 0;
+}
